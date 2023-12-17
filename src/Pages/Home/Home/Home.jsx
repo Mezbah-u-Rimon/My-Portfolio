5c86e393d50939a780638d553e72e5bc421b8414
Muzbah Uddin <rimonmr444@gmail.com>
2023-12-18 00:10:01 +0600

    added skills section in home page

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,13 +1,16 @@
 import About from "../About/About";
 import Banner from "../Banner/Banner";
 import Portfolio from "../Portfolio/Portfolio";
+import Skills from "../Skills/Skills";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <About></About>
+            <Skills></Skills>
             <Portfolio></Portfolio>
+
         </div>
     );
 };
---
+++ src/Pages/Home/Skills/Skills.jsx
@@ -0,0 +1,74 @@
+
+
+const Skills = () => {
+    return (
+        <div className=" px-5 md:px-12 lg:px-24 pb-24 pt-10">
+            <h1 className="text-center text-4xl font-bold text-orange-500 mb-4"> My Expertise
+            </h1>
+            <p className="lg:w-[500px] mx-auto mb-12 text-center">
+                I can work using these technologies. I have completed many projects using them. I have a good understanding of these technologies
+            </p>
+
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://i.ibb.co/YQnvDqM/html-5-2.png" alt="" />
+                    <h3 className="text-xl font-bold">HTML5 </h3>
+                </div>
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://i.ibb.co/yfTk0S7/css-3-2.png" alt="" />
+                    <h3 className="text-xl font-bold">CSS3</h3>
+                </div>
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://i.ibb.co/VxQX5jN/js-1.png" alt="" />
+                    <h3 className="text-xl font-bold"> JavaScript  </h3>
+                </div>
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://i.ibb.co/jGdf1bQ/png-transparent-tailwind-css-hd-logo-thumbnail-removebg-preview.png" alt="" />
+                    <h3 className="text-xl font-bold"> Tailwind  </h3>
+                </div>
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://i.ibb.co/qYjr52t/Bootstrap-front-end-framework-Logo.png" alt="" />
+                    <h3 className="text-xl font-bold"> Bootstrap  </h3>
+                </div>
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://i.ibb.co/Tb9hQfD/science-1.png" alt="" />
+                    <h3 className="text-xl font-bold"> React  </h3>
+                </div>
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://i.ibb.co/QM0FCKq/nodejs-1.png" alt="" />
+                    <h3 className="text-xl font-bold"> Node.Js  </h3>
+                </div>
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://i.ibb.co/hdrvyCL/express-removebg-preview.png" alt="" />
+                    <h3 className="text-xl font-bold"> Express </h3>
+                </div>
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://i.ibb.co/H4rjsqW/mongodb-logo-D13-D67-C930-seeklogo-com.png" alt="" />
+                    <h3 className="text-xl font-bold"> MongoDB </h3>
+                </div>
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://i.ibb.co/FK9fZ4b/png-transparent-firebase-google-google-i-o-icon-thumbnail-removebg-preview.png" alt="" />
+                    <h3 className="text-xl font-bold"> Firebase </h3>
+                </div>
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://i.ibb.co/6mT5kLJ/github.png" alt="" />
+                    <h3 className="text-xl font-bold"> Github </h3>
+                </div>
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://i.ibb.co/ggLcc02/download-removebg-preview-1.png" alt="" />
+                    <h3 className="text-xl font-bold"> Vercel </h3>
+                </div>
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://i.ibb.co/FJmdGwy/images-removebg-preview-2.png" alt="" />
+                    <h3 className="text-xl font-bold"> Netlify </h3>
+                </div>
+                <div className="flex justify-center items-center gap-2 flex-col hover:shadow-lg py-3 hover:shadow-orange-300">
+                    <img className="h-[64px]" src="https://cdn-icons-png.flaticon.com/128/174/174881.png" alt="" />
+                    <h3 className="text-xl font-bold"> Wordpress </h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Skills;
